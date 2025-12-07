4b50dc6033aa237deeda2303d2c892c545265f0b
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2025-12-07 15:46:06 +0330

    4 delet .vs

--- Infra/obj/Debug/net7.0/Infra.AssemblyInfo.cs
+++ Infra/obj/Debug/net7.0/Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7bc32e64fc080da4193ea800685c7d0216c5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be873570a60ffdeebe7f85ad9f245aeea609f4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
