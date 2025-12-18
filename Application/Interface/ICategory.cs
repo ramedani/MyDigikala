2091bb7ab514b4e204eee4fada71e8ea2291f416
ramedanivoorivex-lab <psafilian@gmail.com>
2025-12-18 15:37:56 +0330

    Adjust DataBase

--- Application/Interface/ICategory.cs
+++ Application/Interface/ICategory.cs
@@ -1,5 +1,5 @@
 ﻿using Application.DTO;
-using Microsoft.IdentityModel.Tokens;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Web/Program.cs
+++ Web/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration
+    options.UseNpgsql(builder.Configuration
     .GetConnectionString("a")));
 
 builder.Services.AddScoped<ICategory , CategoryService>();
