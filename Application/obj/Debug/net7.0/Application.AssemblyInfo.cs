4a72faca1d86a7091d0ca753b7be8b6b3f9ef2fc
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2025-12-29 15:45:44 +0330

    heello

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b202c171978d75918d1f89abc762c174e986d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d4483899cc820c9509cb98e4d048fca375e44d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c368fa3e1618ed2cdbe9a312ad371ee4160389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d4483899cc820c9509cb98e4d048fca375e44d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
