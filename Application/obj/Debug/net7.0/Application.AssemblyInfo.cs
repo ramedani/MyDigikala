3e34e93275e02f07d59d6e5f9025586504927e80
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2025-12-29 15:48:06 +0330

    mxsjsfsdf

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bddd055bcea48d826cbfe710cea0f500c4ae522d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac1321f815f8971091e600f4f8778b7aeaf2230")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
