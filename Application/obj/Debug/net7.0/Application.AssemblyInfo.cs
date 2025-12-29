6caa327bdcd3c6fd5f0d3f995610a8dd57902d0c
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2025-12-29 15:48:44 +0330

    samyar1234

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c368fa3e1618ed2cdbe9a312ad371ee4160389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d5604d524c368dbfa52a4d751b9c09778d7c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
