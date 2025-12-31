be4412d5691ea4c2eb985b9a44ecc3496dcca4ce
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2025-12-31 17:55:06 +0330

    assasas

--- Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a37a8c22a410e4f376aa723a739dc99e3912629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb0fab22e03341968f584a7cf140181f1ba3029")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
