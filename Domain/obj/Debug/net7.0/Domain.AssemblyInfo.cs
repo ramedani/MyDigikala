e940e7cedc314d675994a66ac45a013cde83020f
samyarahmadivastani123-bit <samyarahmadivastani123@gmail.com>
2026-01-06 14:24:21 +0330

    h

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b3a31fd64c00b2f97c3a882b0b6251524e699f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd99635a3c0200d58dc9424dae39f0db21d7502")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
